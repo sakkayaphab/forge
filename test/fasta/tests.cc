2553879601cb182fbf36de3800887597bc5c81bb
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2020-01-18 23:31:59 +0700

    update

--- test/fasta/tests.cc
+++ test/fasta/tests.cc
@@ -15,6 +15,8 @@ TEST(FilePathTest, getTestFasta1)
     fastaReader.setFilePath(referencepath);
     fastaReader.setIndexFilePath(referenceindexpath);
     fastaReader.initialize();
+    std::cout << fastaReader.hasIndexFilePath() << std::endl;
+    std::cout << fastaReader.hasFilePath() << std::endl;
     std::string seqrefString = fastaReader.getSeqbyPosition("chr1",0,10);
     ASSERT_EQ("CGAGCCGAAC", seqrefString);
 }
