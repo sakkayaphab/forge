f3ade475b5797b98c67ee81bff164e50c2427096
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2020-01-18 22:10:41 +0700

    update

--- test/fasta/tests.cc
+++ test/fasta/tests.cc
@@ -1,15 +1,13 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "fasta/fastareader.h"
-//#include "caller/readdepthanalysis.h"
-//#include "bwt/bwt.h"
-//#include "caller/stringsearch.h"
-//#include "caller/stringsearchalignment.h"
 
 TEST(FilePathTest, getFilePathName)
 {
-FastaReader fastaReader;
-fastaReader.setFilePath("../../test/data/reference");
-fastaReader.setIndexFilePath("../../test/data/reference" + ".fai");
-fastaReader.initialize();
+    FastaReader fastaReader;
+    fastaReader.setFilePath("../../test/data/reference");
+    fastaReader.setIndexFilePath("../../test/data/reference" + ".fai");
+    fastaReader.initialize();
+    std::string seqrefString = fastareader.getSeqbyPosition("chr1",0,10);
+    ASSERT_EQ("CGAGCCGAAC", seqrefString);
 }
