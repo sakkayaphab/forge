b7a82a1f2527f3b5083691aa560df22237861e50
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2020-01-27 17:51:46 +0700

    update

--- test/sim/tests.cc
+++ test/sim/tests.cc
@@ -7,10 +7,18 @@ TEST(FilePathTest, getFilePathName)
     std::cout << "Hello world" << std::endl;
 }
 
-TEST(FilePathTest, variantrange)
+TEST(FilePathTest, variantrange1)
 {
     VariantRange vr;
     vr.setTextRange("100-1000");
+    ASSERT_EQ(100, vr.getMinLength());
+    ASSERT_EQ(1000, vr.getMaxLength());
+}
+
+TEST(FilePathTest, variantrange2)
+{
+    VariantRange vr;
+    vr.setTextRange(" 100 - 1000 ");
     ASSERT_EQ(100, vr.getMinLength());
     ASSERT_EQ(1000, vr.getMaxLength());
 }
